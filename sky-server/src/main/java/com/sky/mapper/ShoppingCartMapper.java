a9a15bdc71bba586edbfd0d5813ee9aacd9419df
Weng <weng2186505985@gmail.com>
2025-10-07 17:04:04 +0800

    新增缓存套餐代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,25 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    /**
+     *动态条件查询
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart  shoppingCart);
+
+    @Update("update shopping_cart set number =#{number} where id=#{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    @Insert("INSERT INTO shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
+            "VALUES (#{name}, #{image}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
