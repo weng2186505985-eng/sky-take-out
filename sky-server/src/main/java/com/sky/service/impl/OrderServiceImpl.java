dc6bc4b142f74be66264e947da25c06271bd905a
Weng <weng2186505985@gmail.com>
2025-10-10 17:07:33 +0800

    新增来单提醒业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ser.Serializers;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -25,6 +26,7 @@ import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +36,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -52,6 +56,8 @@ public class OrderServiceImpl implements OrderService {
     private ShoppingCartMapper shoppingCartMapper;
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private WebSocketServer  webSocketServer;
 
     /**
      * 用户下单
@@ -377,5 +383,18 @@ public class OrderServiceImpl implements OrderService {
         orders.setDeliveryTime(LocalDateTime.now());
 
         orderMapper.update(orders);
+    }
+
+    public void reminder(Long id) {
+        Orders ordersDB = orderMapper.getById(id);
+        // 校验订单是否存在，并且状态为3
+        if (ordersDB == null ) {
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+        Map map = new HashMap();
+        map.put("type",2);
+        map.put("orderId",id);
+        map.put("content","订单号："+ordersDB.getNumber());
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
     }
 }
--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -1,5 +1,6 @@
 package com.sky.task;
 
+import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -18,7 +20,29 @@ public class OrderTask {
     @Scheduled(cron="0 * * * * ? ")
     public void processTimeoutOrder(){
         log.info("定时处理超时订单：{}", LocalDateTime.now());
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> orderList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+        if(orderList!=null&&orderList.size()>0){
+           for(Orders orders:orderList){
+               orders.setStatus(Orders.CANCELLED);
+               orders.setCancelReason("订单超时，自动取消");
+               orders.setCancelTime(LocalDateTime.now());
+               orderMapper.update(orders);
+           }
+        }
 
+    }
 
+    @Scheduled(cron = "0 0 1 * * ? ")
+    public void processDeliverOrder(){
+        log.info("定时处理派送中订单：{}", LocalDateTime.now());
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+        List<Orders> ordersList=orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+        if(ordersList!=null&&ordersList.size()>0){
+            for(Orders orders:ordersList){
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
     }
 }
